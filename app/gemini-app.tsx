0f3df64738d433ffa4bd56978c2fe0838ed64596
pogo <pogo>
2024-10-14 00:20:05 +1100

    fix the env

--- app/gemini-app.tsx
+++ app/gemini-app.tsx
@@ -2,7 +2,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 
-const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY);
+const genAI = new GoogleGenerativeAI(process.env.NEXT_PUBLIC_GEMINI_API_KEY|| 'failed');
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
 
 
